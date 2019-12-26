9849e9e1f368c652203f13d4116ee96bbf7c63d1
fonces <fonces.xyz@gmail.com>
2019-12-26 17:47:00 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h1>index</h1>
+    <nuxt-link to="/hoge">hoge</nuxt-link>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
