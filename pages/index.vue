6add5403cdb8f708d0727f9214f054f4e4ce704d
fonces <fonces.xyz@gmail.com>
2019-12-27 11:44:07 +0900

    add dynamic route

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,10 @@
   <section>
     <h1>index</h1>
     <nuxt-link to="/hoge">hoge</nuxt-link>
+    <input
+      v-model="fugaId"
+      type="text">
+    <nuxt-link :to="fugaLink">fuga</nuxt-link>
   </section>
 </template>
 
@@ -11,6 +15,18 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  data: () => ({ fugaId: '' }),
+  computed: {
+    fugaLink() {
+      return `/fuga/${this.fugaId}`
+    }
   }
 }
 </script>
+
+<style scoped>
+input {
+  border: solid 1px gray;
+}
+</style>
